ec16e8683e2e1bee75039ff0c7fe9c448f52fb18
Liam GENEBRIER <liam.leksir@gmail.com>
2023-03-28 15:18:50 +0200

    modif du 28/03

--- lancement-quizz.js
+++ lancement-quizz.js
@@ -18,19 +18,54 @@ const changeBouton = () => {
     }
 }
 
-const changeQuestion = () => {
+const play = () => {
+    fetchData('initQuestions')
+    console.log(questions)
+}
 
+// API
+const initThemes = (result) => {
+    for (const theme of result) {
+        document.getElementById('listeTheme').innerHTML += `<option value="${theme.slug}">${theme.libelle}</option>`
+    }
+}
+
+const initQuestions = (result) => {
+    questions = result
+}
+
+const fetchData = (calback) => {
+    switch (calback){
+        case 'initThemes' :
+            endPoint = "http://127.0.0.1:8000/api/quizz"
+            fetch(endPoint).then(res => res.json()).then(result => initThemes(result)).catch(() =>{
+                console.log("erreur")
+            });
+            break;
+        case "initQuestions" :
+            endPoint = `http://127.0.0.1:8000/api/quizz/aleatoire?theme=${slugTheme}&nb=${nbMaxQuestion}`
+            fetch(endPoint).then(res => res.json()).then(result => initQuestions(result)).catch(() =>{
+                console.log("erreur")
+            });
+            break;
+    }
 }
 
 // INIT
-const nbMaxQuestion = 1
-let noQuestion = 1
+let endPoint
+let questions
+let nbMaxQuestion
+let slugTheme
+let noQuestion = 0
 
 document.getElementById("questionQuizz").style.display = "none"
 document.getElementById("btnTerminer").style.display = "none"
 
 document.querySelector("#btnLancerQuizz").addEventListener("click", () => {
+    nbMaxQuestion = document.getElementById("nbQuestion").value
+    slugTheme  = document.getElementById("listeTheme").value
     changeMode()
+    play()
 })
 
 document.querySelector("#btnSuivant").addEventListener("click", () => {
@@ -40,5 +75,7 @@ document.querySelector("#btnSuivant").addEventListener("click", () => {
 document.querySelector("#btnClose").addEventListener("click", () => {
     changeMode()
 })
+
+fetchData('initThemes')
 
 
